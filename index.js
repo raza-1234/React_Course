53e8a750103f00c87593ce6a0f55a13bc41cdd12
raza-1234 <razabutt1863@gmail.com>
2024-08-07 07:47:59 -0700

    playing with react

---
+++ index.js
@@ -0,0 +1,8 @@
+const paraId = document.getElementById("paraId");
+paraId.innerHTML = "hello chansuuu ....";
+
+
+const root = document.getElementById('root')
+    const paragraph = React.createElement('p', {}, 'Hello from me(react) instead of js ...')
+    const reactRoot = ReactDOM.createRoot(root)
+    reactRoot.render( paragraph )
