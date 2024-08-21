61874aa2c67bd89d9104e6a01667ffdc5d9497a8
raza-1234 <razabutt1863@gmail.com>
2024-08-21 06:46:15 -0700

    setup .gitignore and install react, react-dom, parcel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
 const paraId = document.getElementById("paraId");
 paraId.innerHTML = "hello chansuuu ....";
 
-
 const root = document.getElementById('root')
-    const paragraph = React.createElement('p', {}, 'Hello from me(react) instead of js ...')
-    const reactRoot = ReactDOM.createRoot(root)
-    reactRoot.render( paragraph )
+const paragraph = React.createElement('p', {}, 'Hello from me(react) instead of js ...')
+const reactRoot = ReactDOM.createRoot(root)
+reactRoot.render( paragraph )
